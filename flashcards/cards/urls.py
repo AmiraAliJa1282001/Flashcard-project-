34965bcff536032f56d855fc215fc19f21a34cf7
AmiraAliJa1282001 <aa.mm.ii.rr.aa.1282001@gmail.com>
2023-02-05 11:56:29 +0200

    update

---
+++ flashcards/cards/urls.py
@@ -0,0 +1,12 @@
+# cards/urls.py
+from django.urls import path
+# Removed: from django.views.generic import TemplateView
+from . import views
+urlpatterns = [
+    path( "", views.CardListView.as_view(),name="card-list"),
+    path("new", views.CardCreateView.as_view(),name="card-create"),
+    path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update" ),
+    path("delete/<int:pk>" ,views.CardDeleteView.as_view(), name="card-delete"),
+    path("box/<int:box_num>",views.BoxView.as_view(),name="box"),
+
+]
